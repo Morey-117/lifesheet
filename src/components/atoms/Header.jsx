93addfa163c30ff72f5e3c59b729453d3a1cf9e8
Morey-117 <92282778+Morey-117@users.noreply.github.com>
2023-01-22 17:55:55 -0600

    update

--- src/components/atoms/Header.jsx
+++ src/components/atoms/Header.jsx
@@ -5,13 +5,28 @@ function Header(){
     return(
         <header>
             <img src={Foto} alt="Foto de perfil"/>
-            <h1 className="name">Emmanuel</h1>
-            <h1 className="lastname">Moreno Lopez</h1>
+           <info>
+               <h1 className="name">
+                   Emmanuel
+               </h1>
+               <h1 className="lastname">
+                   Moreno Lopez
+               </h1>
+
+           </info>
             <contacts>
-                <h3>Telefono: </h3>
-                <h3>Ubicacion:</h3>
-                <h3>Correo Electronico:</h3>
-                <h3>Repositorio en GitHub:</h3>
+                <h3>
+                    Telefono: +52 961-299-2741
+                </h3>
+                <h3>
+                    Ubicacion:
+                </h3>
+                <h3>
+                    Correo Electronico:
+                </h3>
+                <h3>
+                    Repositorio en GitHub:
+                </h3>
             </contacts>
         </header>
     );
