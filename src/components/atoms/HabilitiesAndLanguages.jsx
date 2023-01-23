5f2213aa367aa2f7716e7fa11d43dd9dbbe0055d
Morey-117 <92282778+Morey-117@users.noreply.github.com>
2023-01-22 20:15:34 -0600

    add

---
+++ src/components/atoms/HabilitiesAndLanguages.jsx
@@ -0,0 +1,35 @@
+import "../../assets/styles/HabilitiesAndLanguages.css"
+function HabilitiesAndLanguages() {
+
+    return(
+    <habilitiesandlanguages>
+            <h1>
+                Habilidades e idiomas
+            </h1>
+        <label>
+            ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        </label>
+        <habilidades>
+            <label>
+                - Manejo de estres
+            </label>
+            <label>
+                - Facilidad para trabajar en equipo
+            </label>
+            <label>
+                -
+            </label>
+        </habilidades>
+        <idiomas>
+            <h3>
+                - Español
+            </h3>
+            <h3>
+                - Ingles
+            </h3>
+        </idiomas>
+    </habilitiesandlanguages>
+    )
+}
+
+export default HabilitiesAndLanguages;
